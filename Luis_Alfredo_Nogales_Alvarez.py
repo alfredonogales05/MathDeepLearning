3f3b139ea0adf71bc10646478d9c873acde3398c
alfredonogales05 <alfredonogales05@hotmail.com>
2024-05-17 13:08:31 -0400

    Math Assigment 1

---
+++ Luis_Alfredo_Nogales_Alvarez.py
@@ -0,0 +1,2 @@
+def hi_my_name_is():
+    return "Luis_Alfredo_Nohales_Alvarez"  # Write your name inside the quotation marks
---
+++ ex1_test.py
@@ -0,0 +1,5 @@
+import Luis_Alfredo_Nogales_Alvarez as name_point
+
+def test_hi_my_name_is():
+    assert len(name_point.hi_my_name_is())  > 1 
+
